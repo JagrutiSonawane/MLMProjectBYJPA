2fe11ecc8abdd79cc0d9532123debe83d2546e1c
unknown <padhyarb@gmail.com>
2022-08-02 18:20:31 +0530

    update test

---
+++ src/test/java/BhagyashriTC/ClassTest1.java
@@ -0,0 +1,17 @@
+package BhagyashriTC;
+
+import org.testng.annotations.Test;
+
+import keyword.UIKeyword;
+
+public class ClassTest1 {
+@Test
+public void m1()
+{
+	UIKeyword.openBrowser("chrome");
+	UIKeyword.launchUrl("https://mlmproductsupply.com/");	
+
+	
+	
+}
+}
